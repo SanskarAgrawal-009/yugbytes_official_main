5a53879c4b949baffff029c2e5b893beab10786b
sanskar agrawal <sanskarofficial512@gmail.com>
2025-05-30 22:13:30 +0530

    no cors set up

--- server/server.js
+++ server/server.js
@@ -19,7 +19,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' ? process.env.FRONTEND_URL : 'https://yugbytes.com',
+  mode: 'no-cors',
+  origin: process.env.NODE_ENV === 'production' ? process.env.FRONTEND_URL:"https://yugbytes.com",
   credentials: true
 }));
 app.use(express.json());
